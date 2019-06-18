a509d9df9d9642b4ebb96d224f31d4f7517bed4b
KIRAN ITALIYA <33808376+Kiran1901@users.noreply.github.com>
2019-06-18 17:56:50 +0530

    dict to list pattern
    improves traversal

--- test1.py
+++ test1.py
@@ -1,41 +1,5 @@
-# string = input()
-# string += " "
-# number_file = open("number.txt",'r')
-# list = []
-# f_list = []
-# temp = ""
-# for x in string:
-#     if(x != " "):
-#         temp = temp + x
-#     else:
-#         list.append(temp)
-#         temp=""
-# c=0
-# for word in list:
-#     #print(list)
-#     for i in range(25):
-#         f_list.append(number_file.readline())
-#     for w in f_list:
-#         if((word + "\n" )==w):
-#             print(list[c+1] + " Noun")
-#     c+=1
-# number_file.close()
-
 import re
 
-# def remove_extra_word(string):
-#     extra_word_file = open("extra_word.txt","r")
-#     line = extra_word_file.readline()[0:-1]
-#     while line:
-#         for word in string.split():
-#             if(string.find(line)!=-1):
-#                 string=string.replace(line,"")
-#                 string = string.replace("  ", " ")
-#                 string=string.strip()
-#                 print("True")
-#         line=extra_word_file.readline()
-#     extra_word_file.close()
-#     print(string)
 
 class remove_extra:
     def remove_extra_word(string):
@@ -78,7 +42,7 @@ class search:
     def base_search(self,word):
         for t in self.f_list:
 
-            pt = re.compile(r'ના$|નું$|ની$|માંથી$|માં$|થી$')
+            pt = re.compile(r'ના$|નું$|ની$|નો$|માંથી$|માં$|થી$')
             if pt.search(word)and len(word)>4:                       #word with suffix (removes suffix)
                 temp=word
                 word=word[:pt.search(word).start()]
@@ -99,66 +63,62 @@ class search:
 class tagger(search):
     def __init__(self,list):
         search.__init__(self)
-        self.list = list
-        self.dict = {}
-        for i in range(0,len(self.list)):
-            self.dict[self.list[i]]=[i,"NaN"]
-        self.totalw = len(self.list)
+        self.word_list = list
+        self.tag_list = []
+        for i in range(0,len(self.word_list)):
+            self.tag_list.append("NaN")
+        self.totalw = len(self.word_list)
         self.NaNcount=0
 
     def base_tag(self):
-        for w in self.list:
-            (self.dict[w])[1] = self.base_search(w)             #searches word in the file and tags
-        return self.dict
+        for i in range(0, self.totalw):
+            self.tag_list[i] = self.base_search(self.word_list[i])             #searches word in the file and tags
+        return self.tag_list
 
     def rule_based_tag(self):
-        lst = list(self.dict.values())
         rn = re.compile(r'\w+\W?\w+\W?નો|\w+\W?\w+\W?ની|\w+\W?\w+\W?નું|\w+\W?\w+\W?ના|\w+\W?\w+\W?ને|\w+\W?\w+\W?થી|\w+\W?\w+\W?માંથી|\w+\W?[^વા]માં')                  #noun
         rv = re.compile(r'હોય|થવું|\w+\W?વ્યા|\w+\W?વ્યું|\w+\W?વવું|\w+\W?\w+\W?વું')                           #verb
 
-        i=flag=0
-        for w,l in self.dict.items():
-
-            if(flag):
-                self.dict[w][1] = "noun"
-                flag=0
+        for i in range(self.totalw):
 
             # Nouns
-            if(rn.search(w) and self.dict[w][1]=="NaN"):
-                self.dict[w][1]="noun"                                   #tags nouns with suffix following pattern
-                if(i<self.dict.__len__()-3):
-                    if(lst[i+1][1]=="NaN" and lst[i+2][1]!="stopper" ):     #noun NaN !stopper => noun noun !stopper
-                        lst[i + 1][1] = "noun"
-                        #print("got it",i)
-                        flag=1
+            if(rn.search(str(self.word_list[i])) and self.tag_list[i]=="NaN"):
+                self.tag_list[i]="noun"                                   #tags nouns with suffix following pattern
+                if(i<self.totalw-3):
+                    if(self.tag_list[i+1]=="NaN" and self.tag_list[i+2]!="stopper" ):     #noun NaN !stopper => noun noun !stopper
+                        self.tag_list[i + 1] = "noun"
 
             #Verbs
-            if (rv.search(w) and self.dict[w][1] == "NaN"):
-                self.dict[w][1]='verb'
+            if (rv.search(str(self.word_list[i])) and self.tag_list[i] == "NaN"):
+                self.tag_list[i]='verb'
 
-            i+=1
+            
+        #return self.tag_list,self.word_list
 
-
-        return self.dict
-
-    def countNaNs(self):
-        for key,value in self.dict.items():
-            if value[1]=='NaN':
+    def countNaNs(self):                                            #counts NaN values in sentence
+        for i in range(self.totalw):
+            if self.tag_list[i]=='NaN':
                 self.NaNcount+=1
         return self.NaNcount
 
     def perc_tagged(self):
-        return (self.totalw-self.countNaNs())/self.totalw
+        return (self.totalw-self.countNaNs())/self.totalw                       #calculates perc
+
+    def print_dict(self):
+        for i in range(0,self.totalw):
+            print(self.word_list[i],":",self.tag_list[i],end="   ")                #prints word:tag pair
+
 
 #MAIN FUNCTION
 
 string_lines = open("input.txt","r").readlines()
 
 for line in string_lines:
-    str = remove_extra.remove_extra_word(line)
-    ls = tockenizer(str).tokenize()
+    string = remove_extra.remove_extra_word(line)
+    word_ls = tockenizer(string).tokenize()
 
-    tg = tagger(ls)
-    ans1 = tg.base_tag()
-    print(tg.rule_based_tag())
-    print("tagged : ",tg.perc_tagged())
+    tg = tagger(word_ls)
+    tag_ls = tg.base_tag()
+    tg.rule_based_tag()
+    tg.print_dict()
+    print("\ntagged : ",tg.perc_tagged())                               #prints tagged perc
